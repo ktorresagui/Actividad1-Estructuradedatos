72a55962f0b2a34c4b52723084da540e011ecdda
ktorresagui <al03022345@tecmilenio.mx>
2025-08-16 16:15:08 -0600

    Update DataTypeExamples.java

--- DataStructure/LinkedList/DataTypeExamples.java
+++ DataStructure/LinkedList/DataTypeExamples.java
@@ -14,10 +14,10 @@ public class DataTypeExamples {
         lista.show();
     }
 
-    public static void testComplexObjects(LinkedList<Contacto> lista) {
-        lista.insertAtLastPosition(new Contacto("Diego Martinez", "Av. Universidad, Monterrey", "7865412001"));
-        lista.insertAtLastPosition(new Contacto("Camila Perez", "Colonia Americana, Guadalajara", "5513024786"));
-        lista.insertAtLastPosition(new Contacto("Armando Betancourt", "Av. Chipinque, Monterrey", "4320167550"));
+    public static void testComplexObjects(LinkedList<Contactos> lista) {
+        lista.insertAtLastPosition(new Contactos("Diego Martinez", "Av. Universidad, Monterrey", "7865412001"));
+        lista.insertAtLastPosition(new Contactos("Camila Perez", "Colonia Americana, Guadalajara", "5513024786"));
+        lista.insertAtLastPosition(new Contactos("Armando Betancourt", "Av. Chipinque, Monterrey", "4320167550"));
         System.out.println("Ejemplo de lista de contactos:");
         lista.show();
     }
